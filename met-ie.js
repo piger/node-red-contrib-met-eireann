5f42c6e7dd2141981fdaf4b2f8f430343a245de3
Daniel Kertesz <daniel@spatof.org>
2021-10-20 23:30:16 +0100

    add plugin code

--- met-ie.js
+++ met-ie.js
@@ -1,11 +1,65 @@
+const http = require("http");
+const xml2js = require("xml2js");
+const { DateTime } = require("luxon");
+
 module.exports = function(RED) {
     function MetIe(config) {
         RED.nodes.createNode(this, config);
+        this.latitude = config.latitude;
+        this.longitude = config.longitude;
+
         var node = this;
 
         node.on('input', function(msg) {
-            msg.payload = "HELLO WORLD";
-            node.send(msg);
+            const now = DateTime.now().startOf("second");
+            const dt = now.toISO({ suppressMilliseconds: true, suppressSeconds: true, includeOffset: false });
+            const url = `http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat={this.latitude};long={this.longitude};from={dt};to={dt}`;
+            http.get(url, (res) => {
+                const { statusCode } = res;
+                if (statusCode !== 200) {
+                    let error = new Error(`Request failed: ${statusCode}`);
+                    console.error(error.message);
+                    res.resume();
+                    return;
+                }
+
+                res.setEncoding("utf8");
+                let rawData = "";
+
+                res.on("data", (chunk) => { rawData += chunk; });
+                res.on("end", () => {
+                    try {
+                        xml2js.parseString(rawData, function(err, result) {
+                            let current = result.weatherdata.product[0].time[0].location[0];
+                            let rainCurrent = result.weatherdata.product[0].time[1].location[0];
+                            let r = {};
+                            r.payload = {
+                                temperature: current.temperature[0].$.value,
+                                windDirection: current.windDirection[0].$.name,
+                                windDirectionDeg: current.windDirection[0].$.deg,
+                                windSpeed: current.windSpeed[0].$.mps,
+                                humidity: current.humidity[0].$.value,
+                                pressure: current.pressure[0].$.value,
+                                cloudiness: current.cloudiness[0].$.percent,
+                                lowClouds: current.lowClouds[0].$.percent,
+                                highClouds: current.highClouds[0].$.percent,
+                                dewPoint: current.dewpointTemperature[0].$.value,
+                                rain: rainCurrent.precipitation[0].$.value,
+                                rainProbability: rainCurrent.precipitation[0].$.probability,
+                            };
+                        });
+                    } catch (e) {
+                        console.error(e.message);
+                    }
+
+                    node.send(r);
+                });
+            }).on("error", (e) => {
+                console.error(`Got error: ${e.message}`);
+            });
+
+            // msg.payload = "HELLO WORLD";
+            // node.send(msg);
         });
     }
 
