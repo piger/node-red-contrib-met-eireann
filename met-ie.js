a3466e3d045d3ff692644cc92133b37fdc972790
Daniel Kertesz <daniel@spatof.org>
2021-10-21 00:44:02 +0100

    better error handling... maybe

--- met-ie.js
+++ met-ie.js
@@ -1,4 +1,4 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     const http = require("http");
     const xml2js = require("xml2js");
     const { DateTime } = require("luxon");
@@ -10,8 +10,8 @@ module.exports = function(RED) {
 
         var node = this;
 
-        node.on('input', function(msg, send, done) {
-            node.status({fill:"blue",shape:"dot",text:"weather.status.requesting"});
+        node.on('input', function (msg, send, done) {
+            node.status({ fill: "blue", shape: "dot", text: "weather.status.requesting" });
 
             const now = DateTime.now().startOf('hour').toISO({ suppressMilliseconds: true, suppressSeconds: true, includeOffset: false });
             const url = `http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=${node.lat};long=${node.long};from=${now};to=${now}`;
@@ -20,45 +20,53 @@ module.exports = function(RED) {
             http.get(url, (res) => {
                 const { statusCode } = res;
                 if (statusCode !== 200) {
-                    let error = new Error(`Request failed: ${statusCode}`);
-                    console.error(error.message);
                     res.resume();
+                    let error = new Error(`Request failed: ${statusCode}`);
                     done(error);
-                    return;
+                } else {
+                    res.setEncoding("utf8");
+                    let rawData = "";
+                    let error;
+
+                    res.on("data", (chunk) => { rawData += chunk; });
+                    res.on("end", () => {
+                        try {
+                            xml2js.parseString(rawData, function (err, result) {
+                                if (err) {
+                                    error = err;
+                                } else {
+                                    let current = result.weatherdata.product[0].time[0].location[0];
+                                    let rainCurrent = result.weatherdata.product[0].time[1].location[0];
+
+                                    msg.payload = {
+                                        temperature: current.temperature[0].$.value,
+                                        windDirection: current.windDirection[0].$.name,
+                                        windDirectionDeg: current.windDirection[0].$.deg,
+                                        windSpeed: current.windSpeed[0].$.mps,
+                                        humidity: current.humidity[0].$.value,
+                                        pressure: current.pressure[0].$.value,
+                                        cloudiness: current.cloudiness[0].$.percent,
+                                        lowClouds: current.lowClouds[0].$.percent,
+                                        highClouds: current.highClouds[0].$.percent,
+                                        dewPoint: current.dewpointTemperature[0].$.value,
+                                        rain: rainCurrent.precipitation[0].$.value,
+                                        rainProbability: rainCurrent.precipitation[0].$.probability,
+                                    };
+                                }
+                            });
+                        } catch (e) {
+                            console.error(e.message);
+                            // done(e);
+                            error = e;
+                        }
+
+                        if (error) {
+                            done(error);
+                        } else {
+                            send(msg);
+                        }
+                    });
                 }
-
-                res.setEncoding("utf8");
-                let rawData = "";
-
-                res.on("data", (chunk) => { rawData += chunk; });
-                res.on("end", () => {
-                    try {
-                        xml2js.parseString(rawData, function(err, result) {
-                            let current = result.weatherdata.product[0].time[0].location[0];
-                            let rainCurrent = result.weatherdata.product[0].time[1].location[0];
-
-                            msg.payload = {
-                                temperature: current.temperature[0].$.value,
-                                windDirection: current.windDirection[0].$.name,
-                                windDirectionDeg: current.windDirection[0].$.deg,
-                                windSpeed: current.windSpeed[0].$.mps,
-                                humidity: current.humidity[0].$.value,
-                                pressure: current.pressure[0].$.value,
-                                cloudiness: current.cloudiness[0].$.percent,
-                                lowClouds: current.lowClouds[0].$.percent,
-                                highClouds: current.highClouds[0].$.percent,
-                                dewPoint: current.dewpointTemperature[0].$.value,
-                                rain: rainCurrent.precipitation[0].$.value,
-                                rainProbability: rainCurrent.precipitation[0].$.probability,
-                            };
-                        });
-                    } catch (e) {
-                        console.error(e.message);
-                        done(e);
-                    }
-
-                    send(msg);
-                });
             }).on("error", (e) => {
                 console.error(`Got error: ${e.message}`);
                 done(e);
