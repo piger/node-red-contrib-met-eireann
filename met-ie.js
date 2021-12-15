9de9d3d7cc7a7eb200636b7c23206b64b90c0b76
Daniel Kertesz <daniel@spatof.org>
2021-12-15 01:51:48 +0000

    reduce indentation in xml parsing function

--- met-ie.js
+++ met-ie.js
@@ -51,7 +51,7 @@ module.exports = function (RED) {
                 let error;
 
                 res.on("data", (chunk) => { rawData += chunk; });
-                
+
                 res.on("end", () => {
                     let opts = {
                         valueProcessors: [ xml2js.processors.parseNumbers ],
@@ -61,33 +61,33 @@ module.exports = function (RED) {
                     try {
                         xml2js.parseString(rawData, opts, function (err, result) {
                             if (err) {
-                                error = err;
-                            } else {
-                                let current = result.weatherdata.product[0].time[0].location[0];
-                                let rainCurrent = result.weatherdata.product[0].time[1].location[0];
-
-                                msg.payload = {
-                                    temperature: current.temperature[0].$['value'],
-                                    windDirection: current.windDirection[0].$['name'],
-                                    windDirectionDeg: current.windDirection[0].$['deg'],
-                                    windSpeed: current.windSpeed[0].$['mps'],
-                                    windSpeedBeaufort: current.windSpeed[0].$['beaufort'],
-                                    globalRadiation: current.globalRadiation[0].$['value'],
-                                    humidity: current.humidity[0].$['value'],
-                                    pressure: current.pressure[0].$['value'],
-                                    cloudiness: current.cloudiness[0].$['percent'],
-                                    lowClouds: current.lowClouds[0].$['percent'],
-                                    mediumClouds: current.mediumClouds[0].$['percent'],
-                                    highClouds: current.highClouds[0].$['percent'],
-                                    dewPoint: current.dewpointTemperature[0].$['value'],
-                                    rain: rainCurrent.precipitation[0].$['value'],
-                                    rainProbability: rainCurrent.precipitation[0].$['probability']
-                                };
+                                throw err;
                             }
+
+                            let current = result.weatherdata.product[0].time[0].location[0];
+                            let rainCurrent = result.weatherdata.product[0].time[1].location[0];
+
+                            msg.payload = {
+                                temperature: current.temperature[0].$['value'],
+                                windDirection: current.windDirection[0].$['name'],
+                                windDirectionDeg: current.windDirection[0].$['deg'],
+                                windSpeed: current.windSpeed[0].$['mps'],
+                                windSpeedBeaufort: current.windSpeed[0].$['beaufort'],
+                                globalRadiation: current.globalRadiation[0].$['value'],
+                                humidity: current.humidity[0].$['value'],
+                                pressure: current.pressure[0].$['value'],
+                                cloudiness: current.cloudiness[0].$['percent'],
+                                lowClouds: current.lowClouds[0].$['percent'],
+                                mediumClouds: current.mediumClouds[0].$['percent'],
+                                highClouds: current.highClouds[0].$['percent'],
+                                dewPoint: current.dewpointTemperature[0].$['value'],
+                                rain: rainCurrent.precipitation[0].$['value'],
+                                rainProbability: rainCurrent.precipitation[0].$['probability']
+                            };
+                            
                         });
                     } catch (e) {
-                        console.error(e.message);
-                        // done(e);
+                        node.error(e.message);
                         error = e;
                     }
 
