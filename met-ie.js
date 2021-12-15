509906dd23b1ac8f772a0b362d4c02f6368a8940
Daniel Kertesz <daniel@spatof.org>
2021-12-15 01:49:05 +0000

    reduce indentation

--- met-ie.js
+++ met-ie.js
@@ -37,62 +37,67 @@ module.exports = function (RED) {
 
             http.get(url, (res) => {
                 const { statusCode } = res;
+
                 if (statusCode !== 200) {
-                    res.resume();
                     let error = new Error(`Request failed: ${statusCode}`);
+                    node.error(error.message);
+                    res.resume();
                     done(error);
-                } else {
-                    res.setEncoding("utf8");
-                    let rawData = "";
-                    let error;
-
-                    res.on("data", (chunk) => { rawData += chunk; });
-                    res.on("end", () => {
-                        let opts = {
-                            valueProcessors: [ xml2js.processors.parseNumbers ],
-                            attrValueProcessors: [ xml2js.processors.parseNumbers ]
-                        }
-
-                        try {
-                            xml2js.parseString(rawData, opts, function (err, result) {
-                                if (err) {
-                                    error = err;
-                                } else {
-                                    let current = result.weatherdata.product[0].time[0].location[0];
-                                    let rainCurrent = result.weatherdata.product[0].time[1].location[0];
-
-                                    msg.payload = {
-                                        temperature: current.temperature[0].$['value'],
-                                        windDirection: current.windDirection[0].$['name'],
-                                        windDirectionDeg: current.windDirection[0].$['deg'],
-                                        windSpeed: current.windSpeed[0].$['mps'],
-                                        windSpeedBeaufort: current.windSpeed[0].$['beaufort'],
-                                        globalRadiation: current.globalRadiation[0].$['value'],
-                                        humidity: current.humidity[0].$['value'],
-                                        pressure: current.pressure[0].$['value'],
-                                        cloudiness: current.cloudiness[0].$['percent'],
-                                        lowClouds: current.lowClouds[0].$['percent'],
-                                        mediumClouds: current.mediumClouds[0].$['percent'],
-                                        highClouds: current.highClouds[0].$['percent'],
-                                        dewPoint: current.dewpointTemperature[0].$['value'],
-                                        rain: rainCurrent.precipitation[0].$['value'],
-                                        rainProbability: rainCurrent.precipitation[0].$['probability']
-                                    };
-                                }
-                            });
-                        } catch (e) {
-                            console.error(e.message);
-                            // done(e);
-                            error = e;
-                        }
-
-                        if (error) {
-                            done(error);
-                        } else {
-                            send(msg);
-                        }
-                    });
+                    return;
                 }
+                
+                res.setEncoding("utf8");
+                let rawData = "";
+                let error;
+
+                res.on("data", (chunk) => { rawData += chunk; });
+                
+                res.on("end", () => {
+                    let opts = {
+                        valueProcessors: [ xml2js.processors.parseNumbers ],
+                        attrValueProcessors: [ xml2js.processors.parseNumbers ]
+                    }
+
+                    try {
+                        xml2js.parseString(rawData, opts, function (err, result) {
+                            if (err) {
+                                error = err;
+                            } else {
+                                let current = result.weatherdata.product[0].time[0].location[0];
+                                let rainCurrent = result.weatherdata.product[0].time[1].location[0];
+
+                                msg.payload = {
+                                    temperature: current.temperature[0].$['value'],
+                                    windDirection: current.windDirection[0].$['name'],
+                                    windDirectionDeg: current.windDirection[0].$['deg'],
+                                    windSpeed: current.windSpeed[0].$['mps'],
+                                    windSpeedBeaufort: current.windSpeed[0].$['beaufort'],
+                                    globalRadiation: current.globalRadiation[0].$['value'],
+                                    humidity: current.humidity[0].$['value'],
+                                    pressure: current.pressure[0].$['value'],
+                                    cloudiness: current.cloudiness[0].$['percent'],
+                                    lowClouds: current.lowClouds[0].$['percent'],
+                                    mediumClouds: current.mediumClouds[0].$['percent'],
+                                    highClouds: current.highClouds[0].$['percent'],
+                                    dewPoint: current.dewpointTemperature[0].$['value'],
+                                    rain: rainCurrent.precipitation[0].$['value'],
+                                    rainProbability: rainCurrent.precipitation[0].$['probability']
+                                };
+                            }
+                        });
+                    } catch (e) {
+                        console.error(e.message);
+                        // done(e);
+                        error = e;
+                    }
+
+                    if (error) {
+                        done(error);
+                    } else {
+                        send(msg);
+                    }
+                });
+
             }).on("error", (e) => {
                 console.error(`Got error: ${e.message}`);
                 done(e);
